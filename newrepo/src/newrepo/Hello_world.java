a88a1310eecd6439921d64a0a2db1f73f449c4a6
Ruman <mohammedRuman1996@gmail.com>
2022-02-05 19:36:26 +0530

    my first commit

---
+++ newrepo/src/newrepo/Hello_world.java
@@ -0,0 +1,10 @@
+package newrepo;
+
+public class Hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("hello_world");
+	}
+
+}
