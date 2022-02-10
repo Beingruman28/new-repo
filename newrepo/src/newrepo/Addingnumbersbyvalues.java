7e0a6b8bc0dfe2e4c3d17eecdafa116f2278832c
Ruman <mohammedruman1996@gmail.com>
2022-02-10 20:10:24 +0530

    commit changes.

---
+++ newrepo/src/newrepo/Addingnumbersbyvalues.java
@@ -0,0 +1,13 @@
+package newrepo;
+
+public class Addingnumbersbyvalues {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 5, num2  = 15 , sum ;
+		sum = num1 + num2 ;
+		System.out.println("sum of two numbers : " + sum);
+
+	}
+
+}
